e0990105af8ed82db26ac4121c78b1087b7be515
seojangwon <seojang980510@naver.com>
2024-12-11 20:31:35 +0900

    feature: [be] livelecture security 기본 코드 작성 및 mysql 연결

---
+++ src/main/java/com/yoganavi/live_lecture/common/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.yoganavi.live_lecture.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                    // 필요한 엔드포인트 권한 설정
+//                .requestMatchers("").hasRole("admin")
+//                .requestMatchers("").authenticated()
+//                .requestMatchers("").authenticated()
+                    .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+}
